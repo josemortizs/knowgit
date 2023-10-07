78f4ea14633768a7f61080c957dc081daa93dc0e
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2023-10-07 18:45:15 +0200

    Initial Commit

---
+++ knowgit Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  knowgit Watch App
+//
+//  Created by Jose Manuel Ortiz Sanchez on 7/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ knowgit Watch App/knowgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  knowgitApp.swift
+//  knowgit Watch App
+//
+//  Created by Jose Manuel Ortiz Sanchez on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct knowgit_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
